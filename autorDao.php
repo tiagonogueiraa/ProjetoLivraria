537f655daa21ceb2d0df86d58b50806f87a12934
Tiago Nogueira <tiagonogueirajf@gmail.com>
2018-03-21 20:26:39 -0300

    criado classes dao e inc de estruturas

---
+++ autorDao.php
@@ -0,0 +1,34 @@
+<?php
+
+require './conexao.inc';
+require './modeloLivraria.inc';
+
+
+class AutorDAO
+{
+ private $con;
+    
+    function AutorDao()
+    {
+        $c = new Conexao();
+        $this->con = $c->getConexao();
+    }
+    
+    public function incluirAutor(Autor $autor)
+    {
+        $sql = $this->con->prepare("insert into autores(nome, email,dt_nasc)values(:nom,:em,:data)");
+        
+        $sql->bindValue(':nom', $autor->getNome());
+        $sql->bindValue(':em', $autor->getEmail());
+        $sql->bindValue(':data', $this->converteDataMysql($autor->getDataNascimento()));
+        $sql->execute();
+    }
+    
+    function converteDataMysql($data)
+    {
+        return date('Y-m-d', $data);
+    }
+}
+
+
+?>
