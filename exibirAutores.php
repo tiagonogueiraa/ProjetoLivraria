a3c425c9f4047ba214490d690dd1780a9f396e93
Tiago Nogueira <tiagonogueirajf@gmail.com>
2018-04-18 17:48:49 -0300

    exibir autores funcionando

--- exibirAutores.php
+++ exibirAutores.php
@@ -1,48 +1,46 @@
-<html>
-    <head>
-        <title>Exibição de Autores</title>
-    </head>
-    <body>
-    <center>
+$<html>
+<head>
+<title>Exibição de Autores</title>
+</head>
+<body>
+	<center><h1>Autores cadastrados</h1>
+	<p>
 
-        <h1>Autores cadastrados</h1>
+<?php
+	
+	function formatarData($data){
+		return date('d/m/Y',$data);
+	}
 
-        <?php
+	session_start();
 
-        function formatarData($data) {
-            return date('d/m/Y', $data);
-        }
+	$autores = $_SESSION['autores'];
 
-        session_start();
+?>
 
-        $autores = $_SESSION['autores'];
-        ?>
-
-        <font face="Tahoma"
-           <table border="1" cellspacing="2" cellpadding="1" width="50%">
-              
-               <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>Data de nascimento</th>
-                <th>Operação</th>
-               </tr>
-               
-           <?php
-           
-                foreach($autores as $autor)
-                {
-                    echo "<tr align='center'";
-                    echo "<td>".$autor->autor_id."</td>";
-                    echo "<td>".$autor->nome."</td>";
-                    echo "<td>".$autor->email."</td>";
-                    //echo ""
-                            
-                            
-                }
-           ?>
-        </center>
-
+	<font face="Tahoma">
+	<table border="1" cellspacing="2" cellspadding="1" width="50%">
+		<tr>
+			<th>ID</th>
+			<th>Nome</th>
+			<th>Email</th>
+			<th>Data de nascimento</th>
+			<th>Operação</th>
+		</tr>
+<?php
+	foreach ($autores as $autor) {
+		echo "<tr align='center'>";
+		echo "<td>".$autor->autor_id."</td>";
+		echo "<td>".$autor->nome."</td>";
+		echo "<td>".$autor->email."</td>";
+		echo "<td>".formatarData(strtotime($autor->dt_nasc))."</td>";
+		echo "<td><a href='controlerAutor.php?opcao=3&id=".$autor->autor_id. "'>Alterar</a>&nbsp;";
+		echo "<a href='controlerAutor.php?opcao=4&id=".$autor->autor_id."'>Excluir</a></td>";
+		echo "</tr>";
+	}
+?>
+	</font>
+	</table>
+	</center>
 </body>
 </html>
