5299b8b4c9399e7e1f45200816046aa22057e130
alunoces <alunoces@L5MAQ23.labcesac.net>
2018-04-25 20:22:18 -0300

    formulario livro e inicio de dao e acrescentado modelo livraria

---
+++ formLivro.php
@@ -0,0 +1,63 @@
+<html>
+    <head>
+        <title>Cadastro livros</title>
+    </head>
+    <body>
+
+        <form action="controlerLivro.php" method="POST">
+            <div>
+                <div>
+                    <div>
+                        <label>Isbn:</label>
+                        <div>
+                            <input type="text" name="isbn">
+                        </div>
+                    </div>
+                    <div>
+                        <label>Título:</label>
+                        <div>
+                            <input type="text" name="titulo">
+                        </div>
+                    </div>
+                    <div>
+                        <label>Número da edição:</label>
+                        <div>
+                            <input type="text" name="edicao_num">
+                        </div>
+                    </div>
+                    <div>
+                        <label>Ano da publicação:</label>
+                        <div>
+                            <input type="text" name="ano_publicacao">
+                        </div>
+                    </div>
+                    <div>
+                        <label>Descrição:</label>
+                        <div>
+                            <input type="text" name="descricao">
+                        </div>
+                    </div>
+                    <div>
+                        <label>Preço:</label>
+                        <div>
+                            <input type="text" name="preco">
+                        </div>
+                    </div>
+                    <input type="hidden" name="opcao" value="1">
+                    <div>
+                        <button type="submit">Enviar</button>
+                    </div>
+                </div>
+            </div>   
+
+
+
+
+
+
+
+        </form>
+
+    </body>
+</html>
+
