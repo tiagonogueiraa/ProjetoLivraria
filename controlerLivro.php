9771c6b5e9da51d84c57ebf4c50b68ccd29bd819
Tiago Nogueira <tiagonogueirajf@gmail.com>
2018-04-29 20:02:07 -0300

    controler livro terminado

---
+++ controlerLivro.php
@@ -0,0 +1,67 @@
+<?php
+    require_once('conexao.inc');
+    require_once('LivroDAO.inc');
+    require_once('modeloLivraria.inc');
+    
+    
+    $opcao = (int)$_REQUEST['opcao'];
+    
+    if($opcao==1){
+	$livro = new Livro($_POST['isbn'], $_POST['titulo'],$_POST['edicao_num'], $_POST['ano_publicacao'], $_POST['descricao'], $_POST['preco']);
+	
+	
+	$livroDAO = new LivroDAO();
+	
+	$livroDao->incluirLivro($livro);
+	
+	header('Location:controlerLivro.php?opcao=2');
+    }
+    
+    if($opcao==2){
+	$livroDAO = new LivroDAO();
+	
+	$lista = $livroDAO->getLivro();
+	
+	session_start();
+	
+	$_SESSION['livros'] = $lista;
+	
+	header('Location:exibirLivros.php');
+    }
+    
+    if(opcao==3){
+	
+	$isbn = $_REQUEST['isbn'];
+	
+	$livroDAO = new LivroDAO();
+	
+	$livro = $livroDAO->getLivro($isbn);
+	
+	session_start();
+	
+	echo $livro->isbn;
+	echo $livro->titulo;
+	
+	$_SESSION['livro'] = $livro;
+	
+	header("Location:formLivroAtualizar.php");
+	
+    }
+    
+    if(opcao==4){
+	
+	$isbn = $_REQUEST['isbn'];
+	
+	$livroDao = new LivroDAO();
+	
+	$livroDAO->excluirLivro($isbn);
+	
+	header("Location:controlerLivro.php?opcao=2");
+	    
+	    
+    }
+    
+    
+    
+
+?>
