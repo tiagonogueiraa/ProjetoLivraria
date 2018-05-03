f314207a5a6cf9775c94acb6c953f212b0c11586
Tiago Nogueira <tiagonogueirajf@gmail.com>
2018-05-03 17:48:02 -0300

    Revert "começado inicio de exibircarrinho"
    This reverts commit a4475de7a0f6b4ba0a05c3a597b51cd0817c36b9.

--- exibirCarrinho.php
+++ exibirCarrinho.php
@@ -1,39 +1,8 @@
-
-<?php
-   
-session_start();
-
-$carrinho = $_SESSION['carrinho'];
-
-$cont = 1;
-$soma = 0;
-
-foreach($carrinho as $item)
-
-{
-
-?>
-
-<tr bgcolor="<?php echo $color ?>" align="center">
-    <td><?php echo $cont?></td>
-    <td><?php echo $item->getId();?></td>
-    <td><?php echo $item->getTitulo();?></td>
-    <td><?php echo $item->getEditora();?></td>
-    <td><?php echo "R$ ".$item->getPreo();?></td>
-    
-    <td><a href="controlerCarrinho.php?opcao=2&index=<?php echo $cont-1 ?>"> Remover Item </a></td>
-
-</tr>
-
-
 <?php
 
-$soma += $item->getPreco();
-$cont++;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-}
-?>
-<tr align="right">
-        <td colspan="5"><font face="Verdana" size="4" color="red">
-        <b>Valor Total = R$<?php echo $soma?></b></font>
-</td></tr>
