26abc99b8a68a6776e570e2eb68de2d7cf3cbf78
Tiago Nogueira <tiagonogueirajf@gmail.com>
2018-05-10 20:24:55 -0300

    comecado exibir carrinho

--- exibirCarrinho.php
+++ exibirCarrinho.php
@@ -1,12 +1,36 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+session_start();
 
-echo $titulo;
+$carrinho = $_SESSION['carrinho'];
+$cont = 1;
+$soma = 0;
 
+foreach($carrinho as $item)
+{
+    
+?>
+            <tr bgcolor="<?php echo $color ?>"align="center">
+                <td><?php echo $cont ?></td>
+                <td><?php echo $item->getId(); ?></td>
+                <td><?php echo $item->getTitulo(); ?></td>
+                <td><?php echo $item->eEditora(); ?></td>
+                <td><?php echo "R$ ".$item->getPreco(); ?></td>
+                <td><a href="controlerCarrinho.php?opcao=2&index=<?php echo $cont-1 ?>">" Remover Item </a> </td>
+           
+            </tr>
+            
+        
+<?php   $soma += $item.getPreco();
+            
+            $cont++;
+    
+} 
+
+        <tr align="right">
+                <td colspan="5"> <font face="Verdana" size="4" color="red">
+                <b>Valor Total =R$<?php echo $soma?></b></font>
+            <td> 
+            </tr>
 ?>
