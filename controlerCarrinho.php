1600a034e117678742e92fc918c4c7b66c60769f
Tiago Nogueira <tiagonogueirajf@gmail.com>
2018-06-13 21:53:43 -0300

    alterado exibir carrinho e alterado controler carrinho

--- controlerCarrinho.php
+++ controlerCarrinho.php
@@ -25,10 +25,23 @@ if($opcao == 1 )
         $_SESSION['carrinho'] = $carrinho;
         
         header("Location:exibirCarrinho.php");
-
-
-
     
+}
+
+if($opcao == 2) // remover do carrinho
+    
+{
+    $index=(int)$_REQUEST['index'];
+    
+    session_start();
+    
+    $carrinho = $_SESSION['carrinho'];
+    
+    unset($carrinho[$index]);
+    
+    $_SESSION['carrinho'] = $carrinho;
+    
+    header("Location:exibirCarrinho.php");
 }
 
 ?>
--- exibirCarrinho.php
+++ exibirCarrinho.php
@@ -1,36 +1,38 @@
 <?php
-
-
 session_start();
 
 $carrinho = $_SESSION['carrinho'];
 $cont = 1;
 $soma = 0;
 
-foreach($carrinho as $item)
-{
-    
-?>
-            <tr bgcolor="<?php echo $color ?>"align="center">
-                <td><?php echo $cont ?></td>
-                <td><?php echo $item->getId(); ?></td>
-                <td><?php echo $item->getTitulo(); ?></td>
-                <td><?php echo $item->eEditora(); ?></td>
-                <td><?php echo "R$ ".$item->getPreco(); ?></td>
-                <td><a href="controlerCarrinho.php?opcao=2&index=<?php echo $cont-1 ?>">" Remover Item </a> </td>
-           
-            </tr>
-            
-        
-<?php   $soma += $item.getPreco();
-            
-            $cont++;
-    
-} 
+foreach ($carrinho as $item) {
+    ?>
+    <tr bgcolor="<?php echo $color ?>"align="center">
+        <td><?php echo $cont ?></td>
+        <td><?php echo $item->getId(); ?></td>
+        <td><?php echo $item->getTitulo(); ?></td>
+        <td><?php echo $item->getEditora(); ?></td>
+        <td><?php echo "R$ " . $item->getPreco(); ?></td>
+        <td><a href="controlerCarrinho.php?opcao=2&index=<?php echo $cont - 1 ?>">" Remover Item </a> </td>
 
-        <tr align="right">
-                <td colspan="5"> <font face="Verdana" size="4" color="red">
-                <b>Valor Total =R$<?php echo $soma?></b></font>
-            <td> 
-            </tr>
+    </tr>
+
+
+    <?php
+    $soma += $item->getPreco();
+
+    $cont++;
+}
 ?>
+
+<tr align="right">
+    <td colspan = "5"> <font face = "Verdana" size = "4" color = "red">
+        <b>Valor Total = R$<?php echo $soma ?></b></font>
+    <td> 
+</tr>
+
+
+<a href="exibirPublicacoes.php">
+    <img src="imagens/botao_continuar_comprando.png">
+</a>
+
